40f6254251dd446780b9b16391bbb9fd2a01cd8b
hanqi <zhouhanqivip@gmail.com>
2017-09-04 18:09:08 +0800

    ORM

--- www/orm.py
+++ www/orm.py
@@ -83,12 +83,28 @@ class ModelMetaClass(type):
             attrs.pop(k)
 
         escaped_fields = list(map(lambda f: '`%s`' % f, fields))
+        attrs['__mappings__'] = mappings
+        attrs['__table__'] = tableName
+        attrs['__primary_key__'] = primaryKey
+        attrs['__fields__'] = fields
+
+        attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ','.join(escaped_fields), tableName)
+        attrs['__insert__'] = 'insert into `%s` (`%s`, %s) values (%s)' % (tableName, primaryKey, ','.join(escaped_fields), create_args_string(len(escaped_fields) + 1))
+        attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
+        attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
+        return type.__new__(cls, name, bases, attrs)
 
 
+def create_args_string(num):
+    L = []
+    for i in range(num):
+        L.append('?')
+    return ','.join(L)
 
 
 
 # user.id = user['id'], (Attributes)getValue, (Attributes)getValueOrDefault
+# .find(), .save(), .update() and .remove()
 class Model(dict, metaclass=ModelMetaClass):
 
     def __init__(self, **kw):
@@ -116,6 +132,77 @@ class Model(dict, metaclass=ModelMetaClass):
                 logging.debug('using default value for %s: %s' % (key, str(value)))
                 setattr(self, key, value)
         return value
+
+    @classmethod
+    async def find(cls, pk):
+        'find object by primary key'
+        rs = await select('%s where `%s=?`' % (cls.__select__, cls.__primary_key__), [pk], 1)
+
+    @classmethod
+    async def findAll(cls, where=None, args=None, **kw):
+        '''
+        find object by where clause
+        select `id`, `name` from `users`
+        - where
+        - orderBy
+        - limit  5, 15  检索记录行6-15
+        '''
+
+        sql = cls.__select__
+        if where:
+            sql.append('where')
+            sql.append(where)
+        if args is None:
+            args=[]
+        orderBy = kw.get('orderBy', None)
+        if orderBy:
+            sql.append('order by')
+            sql.append(orderBy)
+        limit = kw.get('limit', None)
+        if limit:
+            sql.append('limit')
+            if isinstance(limit, int):
+                sql.append('?')
+                args.append(limit)
+            elif isinstance(limit, tuple) and len(limit) == 2:
+                sql.append('?, ?')
+                args.extend(limit)
+            else:
+                raise ValueError('Invalid limit value: %s' % str(limit))
+        rs = await select(' '.join(sql), args)
+        return [cls(**r) for r in rs]
+
+    @classmethod
+    async def findNumber(cls, selectField, where=None, args=None):
+        ' find number by select and where. '
+        sql = ['select %s _num_ from `%s`' % (selectField, cls.__table__)]
+        if where:
+            sql.append('where')
+            sql.append(where)
+        rs = await select(' '.join(sql), args, 1)
+        if len(rs) == 0:
+            return None
+        return rs[0]['_num_']
+
+    async def save(self):
+        args = [self.getValueOrDefault(self.__primary_key__)]
+        args.append(list(map(self.getValueOrDefault, self.__fields__)))
+        rows = await execute(self.__insert__, args)
+        if rows != 1:
+            logging.warning('failed to insert record: affected rows: %s' % rows)
+
+    async def update(self):
+        args = list(map(self.getValue, self.__fields__))
+        args.append(self.getValue(self.__primary_key__))
+        rows = await execute(self.__update__, args)
+        if rows != 1:
+            logging.warning('failed to update by primary key: affected rows: %s' % rows)
+
+    async def remove(self):
+        args = [self.getValue(self.__primary_key__)]
+        rows = await execute(self.__delete__, args)
+        if rows != 1:
+            logging.warning('failed to remove by primary key: affected rows: %s' % rows)
 
 
 class Field(object):
