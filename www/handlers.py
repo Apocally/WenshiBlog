f551d47feba4cd9f288f7556142b6a92915c6e0f
hanqi <zhouhanqivip@gmail.com>
2017-10-10 12:03:15 +0800

    handlers

--- www/handlers.py
+++ www/handlers.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import time, re, hashlib, json, logging, markdown2
+import time, re, hashlib, json, logging
 from coroweb import get, post
 from models import User, Blog, Comment, next_id, Tag, Tagmap
 from apis import APIError, APIValueError, APIPermissionError, Page, APIResourceNotFoundError
 from aiohttp import web
 from config import configs
+import misaka as m
+from pygments import highlight
+from pygments.formatters import HtmlFormatter, ClassNotFound
+from pygments.lexers import get_lexer_by_name
+
 
 __author__ = 'Wenshi'
 
@@ -16,6 +21,23 @@ _RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
 
 COOKIE_NAME = 'awesession'
 _COOKIE_KEY = configs.session.secret
+
+
+class HighlighterRenderer(m.HtmlRenderer):
+    def blockcode(self, text, lang):
+        try:
+            lexer = get_lexer_by_name(lang, stripall=True)
+        except ClassNotFound:
+            lexer = None
+        if lexer:
+            formatter = HtmlFormatter(style='colorful')
+            return highlight(text, lexer, formatter)
+        # default
+        return '\n<pre><code>{}</code></pre>\n'.format(
+                            text.strip())
+
+renderer = HighlighterRenderer()
+md = m.Markdown(renderer, extensions=('fenced-code','tables','highlight','quote',))
 
 
 def check_admin(request):
@@ -84,7 +106,7 @@ async def index(request, *, tag=None):
     # ]
     tags = await Tag.findAll()
     if tag:
-        blogs = await Blog.findIn2Tables('tagmap', 'blogs', 'tagmap.blog_id=blogs.id', 'tagmap.tag_id=?', int(tag))
+        blogs = await Blog.findIn2Tables('tagmap', 'blogs', 'tagmap.blog_id=blogs.id', 'tagmap.tag_id=?', int(tag), orderBy='created_at desc')
     else:
         blogs = await Blog.findAll(orderBy='created_at desc')
     return {
@@ -101,7 +123,7 @@ async def get_blog(id):
     comments = await Comment.findAll('blog_id=?', [id], orderBy='created_at desc')
     for c in comments:
         c.html_content = text2html(c.content)
-    blog.html_content = markdown2.markdown(blog.content)
+    blog.html_content = md(blog.content)
     return {
         '__template__': 'blog.html',
         'blog': blog,
@@ -260,9 +282,6 @@ async def update_blog(id, request, *, name, summary, content, tags):
     return blog
 
 
-
-
-
 @get('/api/blogs/{id}')
 async def api_get_blog(*, id):
     tag_list = []
@@ -272,6 +291,18 @@ async def api_get_blog(*, id):
         tag_list.append(each.tag_id)
     blog['tags'] = tag_list
     return blog
+
+
+@get('/api/comments')
+async def api_comments(*, page='1'):
+    page_index = get_page_index(page)
+    num = await Comment.findNumber('count(id)')
+    # await time.sleep(10)
+    p = Page(num, page_index)
+    if num == 0:
+        return dict(page=p, comments=())
+    comments = await Comment.findAll(orderBy='created_at desc', limit=(p.offset, p.limit))
+    return dict(page=p, comments=comments)
 
 
 @get('/api/blogs')
@@ -286,16 +317,23 @@ async def api_blogs(*, page='1'):
     return dict(page=p, blogs=blogs)
 
 
-@get('/manage/tags')
-async def manage_tags():
-    return  {
-        '__template__': 'manage_tags.html'
-    }
+@get('/api/users')
+async def api_users(*, page='1'):
+    page_index = get_page_index(page)
+    num = await User.findNumber('count(id)')
+    # await time.sleep(10)
+    p = Page(num, page_index)
+    if num == 0:
+        return dict(page=p, users=())
+    users = await User.findAll(orderBy='created_at desc', limit=(p.offset, p.limit))
+    return dict(page=p, users=users)
+
 
 @get('/api/tags')
 async def api_tags():
     tags = await Tag.findAll()
-    return tags
+    return dict(tags=tags)
+
 
 @get('/api/tags/add')
 async def api_add_tags(request, *, tag_name):
@@ -336,6 +374,27 @@ def manage_blogs(*, page='1'):
         'page_index': get_page_index(page)
     }
 
+@get('/manage/users')
+def manage_users(*, page='1'):
+    return {
+        '__template__': 'manage_users.html',
+        'page_index': get_page_index(page)
+    }
+
+
+@get('/manage/comments')
+def manage_comments(*, page='1'):
+    return {
+        '__template__': 'manage_comments.html',
+        'page_index': get_page_index(page)
+    }
+
+@get('/manage/tags')
+async def manage_tags():
+    return  {
+        '__template__': 'manage_tags.html'
+    }
+
 @post('/api/blogs/{id}/comments')
 async def api_create_comment(id, request, *, content):
     user=request.__user__
@@ -349,6 +408,16 @@ async def api_create_comment(id, request, *, content):
     comment = Comment(blog_id=blog.id, user_id=user.id, user_name=user.name, user_image=user.image, content=content.strip())
     await comment.save()
     return comment
+
+
+@post('/api/users/{id}/delete')
+async def api_delete_users(id, request):
+    check_admin(request)
+    u = await User.find(id)
+    if u is None:
+        raise APIResourceNotFoundError('User')
+    await u.remove()
+    return dict(id=id)
 
 
 @post('/api/comments/{id}/delete')
